abe14f4824f3582258e3af6391a72a4599c4377a
e-cagan <emincaganapaydin@gmail.com>
2026-03-13 16:11:52 +0300

    Implented carter dataset parser.

--- main.py
+++ main.py
@@ -3,14 +3,27 @@ Main module to run the algorithms.
 """
 
 from data.generators.synthetic import generate_instance
+from data.parsers.carter_parser import parse_carter
 from src.solvers.cp_solver import solve
 
 
 if __name__ == '__main__':
     # 1. Generate instance
-    instance = generate_instance(n_exams=20, n_timeslots=10, n_rooms=4, n_instructors=8, n_students=60)
+    # instance = generate_instance(n_exams=20, n_timeslots=10, n_rooms=4, n_instructors=8, n_students=60)
 
-    # 2. Solve
+    # Parse the carter dataset
+    instance = parse_carter(
+        crs_path="data/instances/carter/hec-s-92-2.crs",
+        stu_path="data/instances/carter/hec-s-92-2.stu",
+        n_timeslots=18,
+        n_rooms=15,
+        n_instructors=30
+    )
+
+    # Inform user that the dataset instance is loaded
+    print(f"Loaded Carter Instance: {instance}")
+    
+    # 2. Solve the problem (CP-SAT Solver)
     solution = solve(instance=instance)
 
     # 3. Display results
